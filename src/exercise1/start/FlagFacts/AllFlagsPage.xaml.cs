adc093f4bf80710b4dc5f27dd229b7eba7f6efd9
studentsimone <55529034+studentsimone@users.noreply.github.com>
2019-11-28 15:03:25 +0200

    data-listview-assessment

---
+++ src/exercise1/start/FlagFacts/AllFlagsPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using FlagData;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace FlagFacts
+
+{
+
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class AllFlagsPage : ContentPage
+    {
+        public AllFlagsPage()
+
+        {
+            BindingContext = DependencyService.Get<FlagDetailsViewModel>();
+            InitializeComponent();
+        }
+
+        async void ListView_ItemTapped(object sender, ItemTappedEventArgs e)
+        {
+            await this.Navigation.PushAsync(new FlagDetailsPage());
+        }
+    }
+}
